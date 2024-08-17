3b19b43d36504a5d8a9a9c1699289a6cb86866e5
mentalhealf <147326241+mentalhealf@users.noreply.github.com>
2024-08-17 19:00:45 +0300

    adde some

---
+++ index.js
@@ -0,0 +1,52 @@
+// // lesson 2
+// const x = 11;
+// const y = 5;
+// const z = 10;
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+
+// let value = 5;
+// // hard version:
+//  value = value + 10;
+// //easy version:  value += 10;
+// console.log(value);
+
+// const x = 5;
+// const y = 10;
+// const z = 5;
+// console.log("x > y:", x > y);
+// console.log("x < y:", x < y);
+// console.log("x === y:", x === z);
+
+// const valueA = "5";
+// console.log(valueA);
+// console.log(Number(valueA));
+
+// const valueB = "hi";
+// console.log(valueB);
+// console.log(Number(valueB));
+
+// console.log(typeof Number(valueB));
+
+
+//console.log(Number.parseInt("55.5px"));
+
+//console.log(Number.parseFloat("55.55px"));
+
+const validNumber = Number("23");
+console.log(Number.isNaN(validNumber));
+
+const invalidNumber = Number("Hello");
+console.log(Number.isNaN(invalidNumber));
+
+
+
+
+
+
+
+
+
